7b39d9a86148de4d72f13c0dfa55807b29bf61f4
LaurenceRichardson <131921105+LaurenceRichardson@users.noreply.github.com>
2026-05-11 16:45:49 +0100

    added and gate sketch example - 2 buttons and led

---
+++ andGate.ino
@@ -0,0 +1,23 @@
+#define BUTTON_A 7
+#define BUTTON_B 6
+#define LED 8
+int buttonAState=0, buttonBState =0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED, OUTPUT);
+  pinMode(BUTTON_A, INPUT);
+  pinMode(BUTTON_B, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  buttonAState = digitalRead(BUTTON_A);
+  buttonBState = digitalRead(BUTTON_B);
+  if (buttonAState == HIGH && buttonBState == HIGH){
+    digitalWrite(LED, HIGH);
+  }
+  else{
+    digitalWrite(LED, LOW);
+  }
+}
